2acd1e6df52fbb79275d766976597c21a5fd95ea
jwagnerhki <jwagner@mpifr.de>
2021-05-17 16:38:01 +0200

    remove LMT from SCHED block, was scheduled but never observed in EHT2021

--- scripts/extract_vex_portions.sh
+++ scripts/extract_vex_portions.sh
@@ -12,10 +12,18 @@ pushd $SCRIPT_DIR
 function extract_vex_sections() {
 
 	vexname=`basename $1`
+
+	# Get all of SCHED block until end of file
 	sed -n "/\$SCHED;/,/^<eof>/p" $1 > $OUTDIR/sched_$vexname
 
+	# Get all of SOURCE block until FREQ block
 	sed -n "/\$SOURCE;/,/\$FREQ;/p" $1 > $OUTDIR/sources_$vexname
 	sed -i '$ d' $OUTDIR/sources_$vexname  # remove trailing $FREQ; line
+
+	# Remove LMT from SCHED block:
+	# - LMT did not observe EHT2021, yet is in the scheduled-triggered VEX files.
+	# - Remove the station to avoid an issue in DiFX vex2difx ("Developer error: VexData::hasData: cannot find antenna LM")
+	sed -i "s/station\=Lm/\*\*station\=Lm/g" $OUTDIR/sched_$vexname
 }
 
 for vexfile in `ls -1 $VEXDIR/*.vex`; do
