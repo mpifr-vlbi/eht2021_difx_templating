16cf44607b5d03ed44cb89b063bb129eee9546df
jwagnerhki <jwagner@mpifr.de>
2021-12-15 14:50:54 +0100

    add e21a16 to make_initial_clocks.sh make_initial_notes.sh

--- scripts/make_initial_clocks.sh
+++ scripts/make_initial_clocks.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 pushd $SCRIPT_DIR
 
 # 230G
-for expt in e21b09 e21e13 e21a14 e21d15 e21a17 e21e18; do
+for expt in e21b09 e21e13 e21a14 e21d15 e21a16 e21a17 e21e18; do
 	for band in band1 band2 band3 band4; do
 		clockfile=../templates/230G/$band/clocks_$expt.dat
 		if [[ ! -f $clockfile ]]; then
--- scripts/make_initial_notes.sh
+++ scripts/make_initial_notes.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 pushd $SCRIPT_DIR
 
-for expt in e21b09 e21e13 e21a14 e21d15 e21a17 e21e18; do
+for expt in e21b09 e21e13 e21a14 e21d15 e21a16 e21a17 e21e18; do
 	for band in 1 2 3 4; do
 		notesfile=../templates/230G/band$band/notes_$expt.v2d
 		if [[ ! -f $notesfile ]]; then
